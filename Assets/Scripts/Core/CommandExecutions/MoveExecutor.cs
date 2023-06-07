e3aafeb05c96dceb704e691431b9828138eea48e
Aleksey Pasynkov <pasynkov.aleksey@gmail.com>
2023-06-08 01:36:54 +0300

    Navigation
    Added NavMesh, obstacles, agents
    Commands to move and patrol are remade

--- Assets/Scripts/Core/CommandExecutions/MoveExecutor.cs
+++ Assets/Scripts/Core/CommandExecutions/MoveExecutor.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Strategy
 {
     internal class MoveExecutor : CommandExecutor<IMoveCommand>
     {
+        private NavMeshAgent _navMeshAgent;
+        private void Start()
+        {
+            if(!TryGetComponent<NavMeshAgent>(out _navMeshAgent)) _navMeshAgent = gameObject.AddComponent<NavMeshAgent>();
+        }
         public override void ExecuteSpecific(IMoveCommand command)
         {
-            Debug.Log($"{transform.name} moves to {command.Direction}");
+            //Debug.Log($"{transform.name} moves to {command.Direction}");
+            _navMeshAgent.SetDestination(command.Direction);
         }
     }
 }
--- Assets/Scripts/Core/CommandExecutions/PatrolExecutor.cs
+++ Assets/Scripts/Core/CommandExecutions/PatrolExecutor.cs
@@ -1,14 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Strategy
 {
     internal class PatrolExecutor : CommandExecutor<IPatrolCommand>
     {
+        private NavMeshAgent _navMeshAgent;
+        private List<Vector3> _patrolPoints;
+        private int _patrolPointCounter = 0;
+        private Vector3 _target;
+        private void Start()
+        {
+            if (!TryGetComponent<NavMeshAgent>(out _navMeshAgent)) _navMeshAgent = gameObject.AddComponent<NavMeshAgent>();
+            _patrolPoints= new List<Vector3>();
+        }
         public override void ExecuteSpecific(IPatrolCommand command)
         {
-            Debug.Log($"{transform.name} starts on patrol from {transform.position} to {command.PatrolTarget}");
+            _patrolPoints = new List<Vector3>();
+            _patrolPoints.Add(new Vector3(transform.position.x, 0, transform.position.z));
+            _patrolPoints.Add(command.PatrolTarget);
+            ChangeDestination();
         }
+
+        private void ChangeDestination()
+        {
+            _patrolPointCounter++;
+            if(_patrolPointCounter >= _patrolPoints.Count) _patrolPointCounter = 0;
+            _target = _patrolPoints[_patrolPointCounter];
+            _navMeshAgent.SetDestination(_target);
+        }
+
+        private void FixedUpdate()
+        {
+            if(ReachedDestination(new Vector3(transform.position.x, 0, transform.position.z)) && _patrolPoints.Count > 1) ChangeDestination();
+        }
+
+        private bool ReachedDestination(Vector3 position) => Vector3.SqrMagnitude(_target - position) < Mathf.Pow(_navMeshAgent.stoppingDistance, 2)+0.1f;
     }
 }
